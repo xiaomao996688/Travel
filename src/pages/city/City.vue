7873c5a05841bc59dc69fe1d32ae50cc287d30a0
xiaomao <97045924@qq.com>
2018-08-09 09:01:04 +0800

    add search to city

--- src/pages/city/City.vue
+++ src/pages/city/City.vue
@@ -1,15 +1,18 @@
 <template>
 	<div>
 		<city-header></city-header>
+		<city-search></city-search>
 	</div>
 </template>
 <script >
 import CityHeader   from    './components/Header'
+import CitySearch   from    './components/Search'
 import axios		from    'axios'
 export default{
 	name :'Home',
 	components: {
-		CityHeader
+		CityHeader,
+		CitySearch
 	}
 }
 </script>
---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,29 @@
+<template>
+	<div>
+		<div class="search">
+			<input class="search-input" type="text" placeholder="输入城市名或拼音">
+		</div>
+	</div>
+</template>
+<script >
+
+export default{
+	name :'CitySearch',
+	
+}
+</script>
+<style lang="stylus" scoped>
+@import '~styles/varibles.styl'
+	.search
+		height: .72rem
+		padding: 0 .1rem
+		background: $bgColor
+		.search-input
+			box-sizing: border-box
+			height: .62rem
+			width: 100%
+			line-height: .62rem
+			border-radius: .06rem
+			color: #666
+			padding: 0 .1rem
+</style>
