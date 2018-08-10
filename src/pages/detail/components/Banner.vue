ebf83c2f4513fca4c56ba55f7e4d0daea8ef0503
xiaomao <97045924@qq.com>
2018-08-10 19:52:16 +0800

    banner finish

---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,75 @@
+<template>
+	<div>
+		<div class="banner"  @click="handleBannerClick">
+			<img class="banner-img" src="http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_600x330_bf9c4904.jpg" >
+			<div class="banner-info">
+				<div class="banner-title">大连圣亚海洋世界(AAAA景区)</div>
+				<div class="banner-number">
+					<span class="iconfont  banner-icon">&#xe61e;</span>
+					39
+				</div>
+			</div>
+		</div>
+		<common-gallary :imgs="imgs"
+		 v-show="showGallary"
+		 @close="handleCloseGallary"
+		 ></common-gallary>
+	</div>
+</template>
+
+<script>
+import CommonGallary from 'common/gallary/Gallary'
+export default {
+	name: 'DetailBanner',
+	data () {
+		return {
+			showGallary: false,
+			imgs:["http://img1.qunarzz.com/sight/p0/201404/23/04b92c99462687fa1ba45c1b5ba4ad77.jpg_800x800_70debc93.jpg", "http://img1.qunarzz.com/sight/p0/1709/76/7691528bc7d7ad3ca3.img.png_800x800_9ef05ee7.png"]
+		}
+	},
+	components:{
+		CommonGallary
+	},
+	methods: {
+		handleBannerClick () {
+			this.showGallary=true
+		},
+		handleCloseGallary (){
+			this.showGallary= false
+		}
+	}
+}
+</script>
+<style lang="stylus" scoped>
+.banner
+	height:0
+	padding-bottom: 55%
+	overflow:hidden
+	position: relative
+	.banner-img
+		 width: 100%
+	.banner-info
+		display: flex
+		height: .64rem
+		line-height: .64rem
+		position: absolute
+		left: 0
+		right: 0
+		bottom: 0
+		color: #fff
+		background:linear-gradient(top, rgba(0,0,0,0), rgba(0,0,0,.8))
+		.banner-title
+			flex:1
+			padding:0 .2rem
+			font-size: .32rem
+		.banner-number
+			height: .32rem
+			line-height: .32rem
+			margin-top:.14rem
+			padding: 0 .4rem
+			background:rgba(0,0,0,.8)
+			border-radius: .1rem
+			font-size: .24rem
+			.banner-icon
+				font-size: .24rem
+</style>
